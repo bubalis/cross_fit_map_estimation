14bf1340a4a5c891ab6bd14c0772e213e8b196cd
bubalis <benjamintdube@gmail.com>
2026-02-06 13:32:55 -0500

    Added out-of-bag error mae

--- cross_fitting.R
+++ cross_fitting.R
@@ -2,6 +2,44 @@ library(dplyr)
 library(ranger)
 library(rmeta)
 
+
+
+cluster_bootstrap_for_rf <- function(data, group_col, R = 100) {
+ 
+  data$id <- 1:nrow(data)
+  
+  data_samp <- data[, c('id', group_col)]
+  
+  # Get the list of unique group IDs (names of the split list)
+  unique_groups <- unique(data_samp[[group_col]])
+  num_groups <- length(unique_groups)
+  
+  # 2. Initialize a list to store the R bootstrap samples
+  boot_samples <- vector("list", R)
+  
+  # 3. Perform the Bootstrap
+  for (i in 1:R) {
+    # Sample group names WITH replacement
+    # If we have 10 groups, we pick 10 groups, but some might be duplicates.
+    
+    sampled_group_names <- sample(unique_groups, num_groups, replace = TRUE)
+    
+    #Create a vector of the number of times each datapoint is sampled.
+    samples_vector <- table(sampled_group_names) %>% 
+      data.frame() %>%
+      rename(cluster = sampled_group_names ) %>% 
+      mutate(cluster = as.integer(as.character(cluster))) %>% 
+      right_join(data_samp, by = 'cluster') %>% 
+      mutate(Freq = ifelse(is.na(Freq), 0, Freq)) %>% 
+      arrange(id) %>% 
+      pull(Freq)
+    
+    boot_samples[[i]] <-  samples_vector
+  }
+  
+  return(boot_samples)
+}
+
 #' Simulate a Sample from Clustered Data
 #'
 #' This function selects a specific number of clusters from a dataset with probability 
@@ -46,6 +84,8 @@ sim.sample <- function(data, n.clusters, n.per.cluster, cluster_counts){
   }
   pts
 }
+
+
 
 #' Limit and Recycle Cross-Validation Folds
 #'
@@ -312,10 +352,12 @@ x.fit.gdif <- function(full_grd, data, formula, n.folds, cluster_areas,
   return(full_est %>% mutate(n.folds = n.folds))
 }
 
+
+
 #' Run and experiment on the gdif estimator using a random forest model,
 #' cross-fitting, and a 2-stage cluster without-replacement design.
 run.experiment <- function(dataset, formula, n.clusters.sampled,
-                           n.samples.per.cluster, n.folds, 
+                           n.sampled.per.cluster, n.folds, 
                            n.reps = 100){
 
 counts <-  table(dataset$cluster)
@@ -334,6 +376,7 @@ for (i in 1:n.reps){
   samps <- sim.sample(dataset, n.clusters.sampled, 
                       n.sampled.per.cluster, counts)
   
+  
   db_var_est <- cluster_wo_replacement_variance(samps, 'y', cluster_areas)
   db_pred <- mean(samps$y)
     
@@ -344,8 +387,25 @@ for (i in 1:n.reps){
                       lapply(n.folds, sim.fun)
                       )
   
+  
+  
+  clustered_inbag <- cluster_bootstrap_for_rf(samps, 'cluster', 500)
+  
+  rf.mod <- ranger(formula, samps, inbag =clustered_inbag)
+  pred_full <- mean(predict(rf.mod, dataset)$predictions)
+  
+  samps <- samps %>%
+    mutate(pred = rf.mod$predictions, err_oob = pred - y)
+  
+  pred_oob_gdif <- pred_full - mean(samps$err_oob)
+  var_oob_gdif <- cluster_wo_replacement_variance(samps, 'err_oob', cluster_areas)
+  
   new_data <-   bind_rows(new_data,
- data.frame(pred = db_pred, se = sqrt(db_var_est), method = 'db', n.folds = n.clusters.sampled)
+ data.frame(pred = c(db_pred, pred_oob_gdif),
+                     se = c(sqrt(db_var_est), sqrt(var_oob_gdif)), 
+                     method = c('db', 'oob_err_gdif'), 
+                     n.folds = n.clusters.sampled
+            )
  ) %>% mutate(run_i = i)
   
   gdif.sim.res <- gdif.sim.res %>% 
@@ -362,6 +422,9 @@ gdif.sim.res %>%
 
 }
 
+
+#This experiment takes about 1/2 an hour to run on my machine, simpler example
+
 amazonia_vars <- c('SWIR2', 'Terra_PP', 'Prec_dm', 'Elevation', 'Clay', 'x1', 'x2')
 #grdAmazonia <- grdAmazonia %>% mutate(y = AGB)
 formula.amaz <- as.formula(paste('y ~ ', paste(amazonia_vars, collapse = ' + '), sep = '' ) )
@@ -372,7 +435,7 @@ load('data/grd_resampled.rda')
 test <- run.experiment(grd_resampled, 
                        formula.amaz, 
                        n.clusters.sampled = 8,
-                       n.samples.per.cluster = 12, 
+                       n.sampled.per.cluster = 12, 
                        n.folds = c(2,4,8), 
                        n.reps = 2)
 
@@ -380,26 +443,31 @@ test <- run.experiment(grd_resampled,
 res_amaz <- run.experiment(grd_resampled, 
                            formula.amaz, 
                            n.clusters.sampled = 20,
-                           n.samples.per.cluster = 12, 
+                           n.sampled.per.cluster = 12, 
                            n.folds = c(2,5,10,20))
 
 
 
 
-
+print('Coverage summary')
 res_amaz %>% 
+  filter(method %in% c('db', 'simple_avg', 'oob_err_gdif' )) %>%
   group_by(method, n.folds) %>% 
-  summarize_at(vars(coverage_cols), mean) %>%
-  print(n = 25)
+  summarize_at(vars(coverage_cols), mean)
+  
 
-res_amaz %>%  group_by(method, n.folds) %>%
-  summarize(bias = mean(err), v_err = var(err), var_est = mean(se**2),
-            v_t = var(t)) %>% 
-  print(n = 50)
+print('Performance summary')
+res_amaz %>%  
+  filter(method %in% c('db', 'simple_avg', 'oob_err_gdif' )) %>%
+  group_by(method, n.folds) %>%
+  summarize(bias = mean(err), variance_of_errs = var(err), mean_variance_of_estimator = mean(se**2),
+            var_t = var(t), expected_var_t = mean(df /(df-2))) 
 
+write.csv(res_amaz, 'results/amazon_agb_exp.csv')
 
-
-
+#This experiment takes longer to run... more datapoints in the prediction set
+#and a larger 
+#number of covariates in the dataset
 
 load('data/grd_ocs_eu.rda')
 grd.ocs.eu$y <- grd.ocs.eu$ocs
@@ -411,3 +479,19 @@ res_ocs_eu <- run.experiment(grd.ocs.eu,
                              20,
                              12, 
                              n.folds = c(2,5,10,20))
+
+
+print('Coverage summary')
+res_ocs_eu %>% 
+  filter(method %in% c('db', 'simple_avg', 'oob_err_gdif' )) %>%
+  group_by(method, n.folds) %>% 
+  summarize_at(vars(coverage_cols), mean)
+
+print('Performance summary')
+res_ocs_eu %>%  
+  filter(method %in% c('db', 'simple_avg', 'oob_err_gdif' )) %>%
+  group_by(method, n.folds) %>%
+  summarize(bias = mean(err), variance_of_errs = var(err), mean_variance_of_estimator = mean(se**2),
+            var_t = var(t), expected_var_t = mean(df /(df-2))) 
+
+write.csv(res_ocs_eu, 'results/ocs_eu_exp.csv')
